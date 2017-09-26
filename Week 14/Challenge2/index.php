b11e0cb985c764a9ea6a97be4e77f249ed7c1702
EOSHunter <huntercnewton@gmail.com>
2017-09-26 12:54:16 -0400

    week 14

--- Week 14/Challenge2/index.php
+++ Week 14/Challenge2/index.php
@@ -1,33 +1,79 @@
-<?php
+<h1>Doesn't work</h1>
 
-$conn = new mysqli('localhost', 'root', 'root', 'Hunter_Newton') 
-or die ('Cannot connect to db');
+<form action="./index.php" method="GET">
+    <div>
+        <label>Color: </label>
+    </div>
+    <div>
+        <select value="" name="color">
+            <option value="black">Black</option>
+            <option value="blue">Blue</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            <option value="orange">Orange</option>
+            <option value="purple">Purple</option>
+            <option value="pink">Pink</option>
+        </select>
+    </div>
+    <input type="submit" value="Submit" />
+</form>
+<div>
+    <?php
 
-    $result = $conn->query("SELECT Color FROM Challenge2 GROUP BY Color");
+// $conn = new mysqli('localhost', 'root', 'root', 'Hunter_Newton') 
+// or die ('Cannot connect to database');
 
-    echo "<html>";
-    echo "<body>";
-    echo "<label>Color:</label><br>";
-    echo "<select name='id'>";
+    if(!empty($_GET)) {
+        try {
+            echo $_GET['color'];
 
-    while ($row = $result->fetch_assoc()) {
-        unset($id, $name);
-        $id = $row['Color'];
-        $name = $row['Color']; 
-        echo '<option value=" '.$id.' ">'.$name.'</option>';
-}
+            $db = new PDO('mysql:dbname=Hunter_Newton;host=localhost', 'root', 'root');
 
-    echo "</select><br>";
-    echo "<input type='submit' name='submit' value='Save' />";
+        //if there are any errors, message displays
+            $db->setAttribute(PDO::ATTR_ERRMOD,PDO::ERRMODE_EXCEPTION); // THIS IS THE ERROR RIGHT NOW 12:04PM - EDWARD 
 
-    // try {
-    //     $results = $db->query("SELECT Color FROM Challenge2 WHERE Color = :color ");
-    //     echo "Searching";
-    // } catch (Exception $e) {
-    //     echo "No search found"
-    //     exit;
-    // }
+            $color = 'SELECT Name, Description, Price, Color FROM Challenge2 WHERE Color = :color';
 
-    echo "</body>";
-    echo "</html>";
-?>
+        //prepares a statement($color) for execution and returns a statement object
+
+            $prepared = $db->prepare($color);
+            $prepared->bindParam(':color', $_GET['color']);
+
+            $prepared->execute();
+
+        // var_dump(($prepared->fetchAll());
+            echo 'hmmm';
+            echo 'hmm' + $prepared->fetchAll();
+
+            foreach($prepared->fetchAll() as $color) {
+                echo "<p>{$color['Name']}, {$color['Color']}</p>";
+            }
+        //if an exception occurs, catch will catch it(see what I did there, ba dum tss), this code will execute if caught
+        } catch (Exception $e) {
+            echo $e;
+            echo $e->getMessage();
+            exit;
+        }
+    }
+
+//     $result = $conn->query("SELECT Color FROM Challenge2 GROUP BY Color");
+
+//     echo "<html>";
+//     echo "<body>";
+//     echo "<label>Color:</label><br>";
+//     echo "<select name='id'>";
+
+//     while ($row = $result->fetch_assoc()) {
+//         unset($id, $name);
+//         $id = $row['Color'];
+//         $name = $row['Color']; 
+//         echo '<option value=" '.$id.' ">'.$name.'</option>';
+// }
+
+//     echo "</select><br>";
+//     echo "<input type='submit' name='submit' value='Submit' />";
+
+//     echo "</body>";
+//     echo "</html>";
+    ?>
+</div>
